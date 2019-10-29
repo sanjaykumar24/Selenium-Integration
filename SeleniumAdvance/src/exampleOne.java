39302e2f9e9df91cb7f1d759c7fa3ddb1afbcf7e
sanjaykumar24 <sanjaykumarthiagu@gmail.com>
2019-10-29 19:25:09 +0530

    Initial Commit

---
+++ SeleniumAdvance/src/exampleOne.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class exampleOne {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:/Users/HP/Downloads/selenium-java-3.141.59/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.expedia.co.in/");
+	}
+	
+}
